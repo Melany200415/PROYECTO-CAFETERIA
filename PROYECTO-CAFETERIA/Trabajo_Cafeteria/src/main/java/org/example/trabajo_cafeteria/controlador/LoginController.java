71530a15191a34f5618bd9bf9a11474ec4dbba9e
Melany200415 <melany.perugachi@epn.edu.ec>
2026-07-12 22:50:12 -0500

    declaracion de los atributos de fxml de login

--- PROYECTO-CAFETERIA/Trabajo_Cafeteria/src/main/java/org/example/trabajo_cafeteria/controlador/LoginController.java
+++ PROYECTO-CAFETERIA/Trabajo_Cafeteria/src/main/java/org/example/trabajo_cafeteria/controlador/LoginController.java
@@ -1,21 +1,31 @@
 package org.example.trabajo_cafeteria.controlador;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+    public class LoginController {
 
-    @FXML
-    private TextField txtUsuario;
+        @FXML
+        private TextField txtUsuario;
 
-    @FXML
-    private TextField txtContrasena;
+        @FXML
+        private PasswordField txtContrasena;
 
-    @FXML
-    public void initialize() {
+        @FXML
+        private Label lblMensaje;
+
+        @FXML
+        private void iniciarSesion() {
+
+            String usuario = txtUsuario.getText();
+            String contrasena = txtContrasena.getText();
+
+            if(usuario.equals("admin") && contrasena.equals("1234")){
+                lblMensaje.setText("Bienvenido");
+            }else{
+                lblMensaje.setText("Credenciales incorrectas");
+            }
+        }
     }
-
-    @FXML
-    private void iniciarSesion() {
-    }
-}
